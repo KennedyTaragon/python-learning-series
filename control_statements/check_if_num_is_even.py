5c97e3ffa99520cc7feeaaf9c583ee4e983095c9
KennedyTaragon <kennedykoech048@gmail.com>
2025-10-27 13:43:19 +0300

    Check if a number is even or odd

---
+++ control_statements/check_if_num_is_even.py
@@ -0,0 +1,12 @@
+def is_even(number):
+    if number % 2 == 0:
+        print(f"The number {number} is EVEN")
+    else:
+        print(f"The number {number} is ODD")
+
+
+
+
+
+number = int(input("Enter your a number: "))
+is_even(number)
--- control_statements/username_length.py
+++ control_statements/username_length.py
@@ -2,7 +2,7 @@ username = input("Enter your Desired Username: ")
 
 def hint_username(username):
     if len(username) < 5:
-        print("Invalid username. Must be at least 3 characters long")
+        print("Invalid username. Must be at least 5 characters long")
     else:
         print("Valid username")
 
